eb90a53d9578411a51ce51cbb9025541b5a9ab36
armandopato <armauv@gmail.com>
2020-11-10 16:49:19 -0600

    Initial files

---
+++ src/main/java/org/mascotadopta/adoptionsplatform/MascotadoptaRestApiApplication.java
@@ -0,0 +1,13 @@
+package org.mascotadopta.adoptionsplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MascotadoptaRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MascotadoptaRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mascotadopta/adoptionsplatform/MascotadoptaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mascotadopta.adoptionsplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MascotadoptaRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
