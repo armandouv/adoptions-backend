037d074cdd0335df99a036a33bb9901b495511ab
armandopato <armauv@gmail.com>
2020-12-23 13:11:31 -0600

    Add UserDetailsService with DB retrieval

---
+++ src/main/java/org/mascotadopta/adoptionsplatform/auth/ApplicationUserDetailsService.java
@@ -0,0 +1,29 @@
+package org.mascotadopta.adoptionsplatform.auth;
+
+import org.mascotadopta.adoptionsplatform.users.User;
+import org.mascotadopta.adoptionsplatform.users.UsersRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ApplicationUserDetailsService implements UserDetailsService
+{
+    private final UsersRepository usersRepository;
+    
+    public ApplicationUserDetailsService(UsersRepository usersRepository)
+    {
+        this.usersRepository = usersRepository;
+    }
+    
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException
+    {
+        Optional<User> user = this.usersRepository.findByEmail(email);
+        if (user.isEmpty()) throw new UsernameNotFoundException("User does not exist");
+        return user.get();
+    }
+}
--- src/main/java/org/mascotadopta/adoptionsplatform/users/User.java
+++ src/main/java/org/mascotadopta/adoptionsplatform/users/User.java
@@ -1,21 +1,23 @@
 package org.mascotadopta.adoptionsplatform.users;
 
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
+import java.util.Collection;
 
 /**
  * A registered user.
  */
 @Entity
-public class User
+public class User implements UserDetails
 {
     /**
-     * Represents whether a User has already
-     * confirmed their email or not.
+     * Represents whether a User has already confirmed their email or not.
      */
     private boolean isEmailConfirmed = false;
     
@@ -73,5 +75,47 @@ public class User
     public void setEmailConfirmed(boolean emailConfirmed)
     {
         isEmailConfirmed = emailConfirmed;
+    }
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities()
+    {
+        return null;
+    }
+    
+    @Override
+    public String getPassword()
+    {
+        return this.password;
+    }
+    
+    @Override
+    public String getUsername()
+    {
+        return this.email;
+    }
+    
+    @Override
+    public boolean isAccountNonExpired()
+    {
+        return false;
+    }
+    
+    @Override
+    public boolean isAccountNonLocked()
+    {
+        return false;
+    }
+    
+    @Override
+    public boolean isCredentialsNonExpired()
+    {
+        return false;
+    }
+    
+    @Override
+    public boolean isEnabled()
+    {
+        return this.isEmailConfirmed;
     }
 }
