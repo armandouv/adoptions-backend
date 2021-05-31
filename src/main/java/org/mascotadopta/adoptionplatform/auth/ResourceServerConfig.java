40d0947dd06532155661736ae187b0a36814fd73
armandopato <armauv@gmail.com>
2021-05-30 20:38:14 -0500

    Add CORS config to allow all origins

--- src/main/java/org/mascotadopta/adoptionplatform/auth/ResourceServerConfig.java
+++ src/main/java/org/mascotadopta/adoptionplatform/auth/ResourceServerConfig.java
@@ -1,9 +1,13 @@
 package org.mascotadopta.adoptionplatform.auth;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter
@@ -26,4 +30,11 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter
                 .jwt();
     }
     
+    @Bean
+    CorsConfigurationSource corsConfigurationSource()
+    {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
 }
